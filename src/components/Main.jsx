ed5518bd29ff44dee5777d3f82f4a677c735123e
deekshithm0 <deekshithm0@gmail.com>
2024-12-15 22:57:04 +0530

    form actions started

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Main = () => {
+  const [isFocused, setIsFocused] = useState(false)
+
   return (
     <main className='main-content w-full h-screen grid place-items-center bg-gradient-to-b from-zinc-950 to-zinc-500'>
-      <div className='w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 items-center px-4 gap-8'>
-        <div className='w-full h-80 flex items-center text-center text-wrap p-4  '>
-          <h1 className='headline-1 text-5xl md:text-6xl font-bold'>Choose flavors, relish the recipe!</h1>
+      <div className='w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 items-center px-4 gap-8 '>
+        <div
+          className={` w-full h-80 top-10 left-0 transition-all duration-300 ease-in-out flex items-center text-center text-wrap p-4 ${
+            isFocused ? 'opacity-0 invisible md:opacity-100 md:visible' : 'opacity-100 visible'
+          } `}
+        >
+          <h1 className='headline-1 text-5xl md:text-6xl font-bold '>
+            Choose flavors, relish the recipe!
+          </h1>
         </div>
-        <form action='' className='px-4 grid gap-7 pt-32 md:pt-0'>
+        <form action='' className='relative z-10 grid gap-7 px-4 w-full md:pt-0 pt-32' onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}>
           <input
             type='text'
             placeholder='eg. Onion'
             aria-label='Add ingredient'
             className='px-4 py-2 rounded-md hover:bg-zinc-400 transition-colors '
+            
           />
-          <button className='bg-sky-400 rounded-md w-full h-8 text-zinc-900 text-sm px-2'>Add ingredient</button>
+          <button className='bg-sky-400 rounded-md w-full h-8 text-zinc-900 text-sm px-2'>
+            Add ingredient
+          </button>
         </form>
       </div>
     </main>
